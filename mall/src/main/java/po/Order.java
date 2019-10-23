ec157022d591ee465cf86af5e5d11dc686c82263
TGFC065 <tgfc065@tgfc.tw>
2019-10-23 16:14:06 +0800

    基本配置 0.0.1

---
+++ mall/src/main/java/po/Order.java
@@ -0,0 +1,14 @@
+package po;
+
+import javax.persistence.*;
+
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
